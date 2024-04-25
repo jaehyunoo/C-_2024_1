90f8feb53c17cb9b577944e59cb93c682e52073b
jaehyunoo <142777825+jaehyunoo@users.noreply.github.com>
2024-04-26 04:25:23 +0900

    Add files via upload

--- 0418_2Program.cs
+++ 0418_2Program.cs
@@ -5,15 +5,14 @@ internal class Program
 {
      class Product
     {
-        private static int counter = 0;
+        private static int counter = 1;
         private int id;
         private string name;
         private int price;
 
         public Product(string name, int price)
         {
-            Product.counter++;
-            this.id = counter;          
+            this.id = counter++;          
             this.name = name;
             this.price = price;
         }
@@ -21,13 +20,13 @@ internal class Program
         public int getId() { return id; }
         public string getName() { return name; }
         public int getPrice() { return price; }
-        public static int getCounter() { return counter; }
 
 
 
     }
 
- 
+
+
     private static void Main(string[] args)
     {
         List<Product> products = new List<Product>();
@@ -35,8 +34,8 @@ internal class Program
         while (true)
         {
             Console.Write("상품 이름을 입력하시오 (종료 : exit) : ");
-            string name = Console.ReadLine();
-            if (name == "exit")
+            string pname = Console.ReadLine();
+            if (pname == "exit")
             {
                 Console.WriteLine("프로그램 종료");
                 break;
@@ -44,28 +43,15 @@ internal class Program
             Console.Write("상품의 가격을 입력하시오 : ");
             int price = int.Parse(Console.ReadLine());
 
-            products.Add(new Product(name, price));
-
-
+            products.Add(new Product(pname, price));
         }
 
-        foreach(var product  in products)
+        Console.WriteLine("\n입력된 제품 목록:");
+        foreach (var product in products)
         {
-            Console.WriteLine( product.getName() + " : " + product.getPrice());;
+            Console.WriteLine($"{product.getId()} - {product.getName()} : {product.getPrice()}");
         }
 
-
-
-        Product productA = new Product("감자",2000);
-        Product productB = new Product("고구마", 3000);
-
-        Console.WriteLine(productA.getId() + " " + productA.getName() + " = " + productA.getPrice());
-        Console.WriteLine(productB.getId() + " " + productB.getName() + " = " + productB.getPrice());
-        Console.WriteLine(productB.getId() + "개 생성");
-        Console.WriteLine(Product.getCounter());
-
-
-
-
+        Console.WriteLine($"\n총 {products.Count}개의 제품이 생성되었습니다.");
     }
 }
